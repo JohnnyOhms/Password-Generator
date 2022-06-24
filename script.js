7e6b519dc995a2a4ba25678cd420c375e6070a4d
JohnnyOhms <officialjohn662@gmail.com>
2022-06-25 00:24:32 +0100

    showpassword function

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const range = document.getElementById('range');
 let minValue = document.getElementById('min');
 
 range.addEventListener("input", ()=>{
-    minValue.innerHTML = range.value;
+    minValue.innerHTML =  range.value;
+    range.value < 10 ? minValue.innerHTML = '0' + range.value: minValue;
 })
 
 let password = {
@@ -33,7 +34,15 @@ generate.addEventListener("click", ()=>{
     display.innerHTML = showPassword(data);
 })
 
-function showPassword(data){
-
+function showPassword(d){
+    let disPlay = "";
+    for (let i = 0; i < 5; i++) {
+       disPlay += d.charAt(Math.floor(Math.random() * d.length))
+        // return disPlay;
+        console.log(disPlay);
+    }
+    // display.innerHTML = disPlay;
+ 
+    // console.log(disPlay);
 }
 
