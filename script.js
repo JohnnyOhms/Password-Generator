adaf897ae8e5b100278198b213e34b4bdc1dc309
JohnnyOhms <officialjohn662@gmail.com>
2022-06-24 19:18:30 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const range = document.getElementById('range');
+let minValue = document.getElementById('min');
 
 range.addEventListener("input", ()=>{
-    let minValue = document.getElementById('min');
     minValue.innerHTML = range.value;
 })
 
@@ -11,3 +11,29 @@ let password = {
     numbers : "0123456789",
     symbols : "!@#$%^&*_-+"
 }
+
+const display = document.querySelector(".display")
+const generate = document.querySelector(".btn");
+const uppperCase = document.querySelector("#uppercase");
+const lowerCase = document.querySelector("#lowercase");
+const numbers = document.querySelector("#numbers");
+const symbols = document.querySelector("#symbols");
+
+display.innerHTML = "";
+
+generate.addEventListener("click", ()=>{
+    display.innerHTML = "";
+    let data = "";
+    uppperCase.checked ? data += password.upperCase : data;
+    lowerCase.checked ? data += password.lowerCase : data;
+    numbers.checked ? data += password.numbers : data;
+    symbols.checked ? data += password.symbols : data;
+    console.log(data);
+
+    display.innerHTML = showPassword(data);
+})
+
+function showPassword(data){
+
+}
+
