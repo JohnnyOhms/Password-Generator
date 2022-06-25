59ef326e19d39db14a1d887d179c51abecd930e7
JohnnyOhms <officialjohn662@gmail.com>
2022-06-25 23:20:55 +0100

    copy function enabled

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const range = document.getElementById('range');
-let minValue = document.getElementById('min');
-let disPlay = "";
-let data = "";
+let min = document.getElementById('min');
+let empty = "";
+let minValue = min
 
 range.addEventListener("input", ()=>{
+    
     minValue.innerHTML =  range.value;
-    range.value < 10 ? minValue.innerHTML = '0' + range.value: minValue;
+    range.value < 10 ? min.innerHTML = '0' + range.value: min;
+
 })
 
 let password = {
@@ -22,22 +24,54 @@ const lowerCase = document.querySelector("#lowercase");
 const numbers = document.querySelector("#numbers");
 const symbols = document.querySelector("#symbols");
 
-display.innerHTML = "";
-
 generate.addEventListener("click", ()=>{
-    display.innerHTML = "";
-    uppperCase.checked ? data += (password.upperCase) : "";
-    lowerCase.checked ? data += (password.lowerCase) : "";
-    numbers.checked ? data += (password.numbers) : "";
-    symbols.checked ? data += (password.symbols) : "";
-    console.log(data);
-    console.log(data.length);
 
-    // for (let i = 0; i < 5; i++) {
-    //    disPlay += data.charAt(Math.floor(Math.random() * data.length))
-    // }
-    // console.log(disPlay);
+    if (minValue.textContent == 0){
+        alert('Select a range number')
+    }else{
+        display.innerHTML = "";
+        
+        if(uppperCase.checked || lowerCase.checked || numbers.checked || symbols.checked ){
+            let data = empty;
+            uppperCase.checked ? (data += password.upperCase) : "";
+            lowerCase.checked ? (data += password.lowerCase) : "";
+            numbers.checked ? (data += password.numbers) : "";
+            symbols.checked ? (data += password.symbols) : "";
+            generatePassword(data)
+            
+        }else{
+            alert("Atleast select a setting")
+        }
+    }
+
 })
+
+function generatePassword(data) {
+
+    let dis_play = ""
+    for (let i = 0; i < minValue.textContent; i++){
+        dis_play += data.charAt(Math.floor(Math.random() * data.length))
+    }
+
+    display.innerHTML = dis_play;
+    console.log(dis_play);
+    
+}
+
+display.addEventListener("click", function(){
+    
+    let textArea = document.createElement("textarea");
+    textArea.value = display.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove()
+    alert("Password Copied");
+
+})
+
+
+
 
 
 
